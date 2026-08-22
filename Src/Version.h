42689ddcb1afa2b29546f849ea7541f2a7820e35
gx <xuan.guo@atomdatatech.com>
2026-08-23 01:59:47 +0800

    v0.1.1

--- Src/Version.h
+++ Src/Version.h
@@ -2,7 +2,7 @@
 
 // Single source of truth for the app version. CI tags should match
 // (v0.1.0 => APP_VERSION "0.1.0").
-#define APP_VERSION "0.1.0"
+#define APP_VERSION "0.1.1"
 #define APP_REPO_URL "https://github.com/kkzi/TsFileViewer"
 #define APP_RELEASES_API "https://api.github.com/repos/kkzi/TsFileViewer/releases/latest"
 #define APP_RELEASES_URL "https://github.com/kkzi/TsFileViewer/releases/latest"
